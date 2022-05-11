0b80143f7e3514c7cb42df4d26bb4dbd25e8c095
Hiren1993 <hirenchauhan7@gmail.com>
2022-05-11 15:06:46 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,490 @@
+<?php
+	//echo 'Hello! There!' .'<br />';
+	//print 'Hello!!!! World!!!!';
+
+	//Create our variable
+	//$name = "Hiren";
+
+	//$name="Test";
+
+	//$name="Test2";
+	//echo $name;
+	//echo "My name is $name"; 
+
+	//$address="Test address";
+
+	//echo $name . ' ' . $address;
+
+	//echo 'It\'s okay';
+	//echo "My name is \"Hiren\"";
+	//echo $name;
+
+
+	//Constant
+	//define('NAME','HIREN');
+
+	//NAME="Test";
+	/*$test = "This is testing string";
+	//String Length
+	echo strlen($test) . '<br/>';
+
+	//String to UpperCase
+	echo strtoupper($test) . '<br/>';
+
+	//String to Lowercase
+	echo strtolower($test) . '<br/>';
+
+	//String Replace
+	echo str_replace('T','Y',$test) .'<br/>';
+
+	//Numbers
+	$radius=25;*/
+	//$pi = 3.14;
+
+	//echo $pi * $radius ;
+
+
+	//BODMAS (Bracket, Any Power,Divide,Multiplication,Addition,Substraction)
+
+	//echo round(2 * (4+9) / 3,2); 
+	//Increment operation
+	//$radius++;
+	//echo $radius;
+
+	//Decrement Operation
+	//$radius--;
+	//echo $radius;
+/*
+	$age=30;
+	//Shorthand operators
+	//$age +=10; // $age = $age + 10;
+
+	$age-=10;
+	echo $age; */
+	//$name='Hiren';
+	//echo 'This is test string' . ' ' .$name;
+
+	//$pi = 3.14;
+	//echo ceil($pi);
+	//echo pi();
+
+	//Array
+	/*$peopleOne = ['John','Dane','Peter'];
+	$peopleTwo = ['Lorem','Ipsum','Hiren'];
+
+	$peopleThree = ['Lorem','Ipsum',30];
+	print_r($peopleThree);*/
+	/*$peopleName[1] = 'Jane';
+	print_r($peopleName);*/
+	//echo $peopleName[0];
+	/*echo $peopleName[0] . '<br />';
+	echo $peopleName[1] . '<br />';
+	echo $peopleName[2] . '<br />';*/
+
+	//$ages = [10,20,30];
+	//print_r($ages);
+
+	//$peopleTwo=['test1','test2','test3'];
+	//$peopleName = array('test1','test2','test3');
+	//print_r($peopleName);
+
+	//$peopleName[0]='Hiren';
+	
+	//$peopleName[]='Gary';
+	
+	//array_push($peopleName,'Leo');
+
+	//print_r($peopleName);
+
+	//$peopleThree = array_merge($peopleOne,$peopleTwo);
+	//print_r($peopleThree);
+
+	//echo count($peopleThree);
+
+	//Merge arrays
+
+	//Associate Arrays
+	//$testOne = ['test1'=>'black','test2'=>'blue'];
+	//echo $testOne['test2'];
+
+	//Multi-dimensional array
+	/*$blogs = [
+		['test1','lorem ipsum'],
+		['test2','abcxyz']
+	];*/
+
+	/*$popped = array_pop($blogs);
+	print_r($popped);*/
+	//print_r($blogs);
+
+
+	//$password='Test12345' . \\n . 'This is test string.';
+	/*$password="Test123456 \n This is a test string.";
+	echo $password;*/
+
+	//echo nl2br("this is test string \n this is another test string");
+
+	//$numbers = [1,22,5,7,25];
+	//Sorting ascending order
+	//sort($numbers);
+
+	//Sorting desceding order
+	//rsort($numbers);
+
+	//print_r($numbers);
+
+	//$testArray = array('test1',50,false);
+	//print_r($testArray);
+
+	//$str = " Hello World! ";
+	
+	//for boolean browser will display true it will display 1 and for false it will be blank
+	//echo "42"==42;
+
+	//echo 'john' == 'john';
+
+	//loose comparison vs strict comparison
+
+	//$test="this is a testing string";
+	//echo strrpos($test,'i');
+	//echo substr($test,5,2);
+
+	/*$test='val1';
+	switch($test){
+		case 'val1':
+			//Do this
+			echo 'this is val1';
+			break; 
+		case 'val2':
+			//Do this
+			echo 'this is val2'; 
+			break;
+		case 'val3':
+			//Do this
+			echo 'this is val3'; 
+			break;
+		default:
+			echo 'val is something else.'; 
+			break;
+	}*/
+
+	//explode and implode function
+	/*$s1 = "Mon-Tue-Wed-Thu-Fri";
+	$day_array = explode('-',$s1);
+
+	$s2= implode(',',$day_array);
+	print_r($s2);*/
+	//echo $day_array[0];
+
+	//$array1 = [10,20,30,40,50];
+	//$array2 = [10,20,30,60,70,80,90,100];
+	//print_r(array_combine($array1, $array2));
+
+	//array_combine()
+
+	/*$array2=['red','blue','yellow'];
+	$array1=[10,20,30];
+	print_r(array_combine($array1, $array2));*/
+
+	//$input = array("a", "b", "c", "d", "e");
+	//print_r(array_slice($input,2));
+	//$output = array_slice($input, 2);      // returns "c", "d", and "e"
+	//$output = array_slice($input, -2, 1);  // returns "d"
+	//$output = array_slice($input, 0, 3);   // returns "a", "b", and "c"
+
+	//$input = array("red", "green", "blue", "yellow");
+	//array_splice($input, 2,1);
+	//print_r($input);	
+
+	//$array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
+
+	//$key = array_search('green', $array); // $key = 2;
+	//$key = array_search('red', $array);   // $key = 1;
+	//$name="This is string.";
+	//echo $name;
+
+
+	//$name2 =' john ';
+
+	//$test = "My name is $name2";
+	//echo $test;
+	//echo $name . $name2;
+
+
+	//echo strlen($name2);
+
+	//echo strtoupper($test);
+
+	//echo strtolower($test);
+
+	//echo str_replace('M', 'Y', $test);
+
+	//echo ucfirst($name2);
+
+	//echo ucwords($test);
+
+
+	//Contsant
+	//define('NAME','HIREN');
+
+	/*trim($name2);
+	ltrim($name2);
+	rtrim($name2);*/
+
+	//$radius=25;
+	//$pi=3.14;
+
+	//echo $radius * $pi * 2;
+
+	//BODMAS Rule
+	//echo round(2 * (4+9) / 3); 
+
+
+	//$radius++;
+	//echo $radius;
+
+	//$radius--;
+	//echo $radius;
+
+	//$age=10;
+	//$age +=  10;
+	//echo $age;
+
+	//echo pi();
+
+	/*$peopleOne = ['test1','test2','test3'];
+	echo $peopleOne[1];*/
+
+	/*$peopleTwo = array('test4','test5');
+	print_r($peopleTwo);*/
+
+	//$ages=[20,5,8,10,2];
+	//$ages2 = [40,50,60,70,80,90];
+	/*rsort($ages);
+	print_r($ages);*/
+
+	//$ages[]=60;
+	//array_push($ages,60);
+
+	//$age3= $ages + $ages2;
+	//print_r($age3);
+	//print_r($ages);
+	//var_dump();
+	//$ages3 = array_merge($ages,$ages2);
+	//print_r($ages3);
+
+	//print_r(array_diff($ages,$ages2));
+	//print_r(array_intersect($ages, $ages2));
+
+	//Associative Array
+	/*$testOne = ['test1'=>'black','test2'=>'blue'];
+	echo $testOne['test1'];*/
+
+
+	//echo nl2br("This is \n testing.");
+
+	//Multi-dimensional array
+	/*$blogs=[
+		['test1','lorem ipsum'],
+		['test2','abcxyz']
+	];*/
+	//print_r($blogs[1][1]);
+
+	//$input = array("a", "b", "c", "d", "e");
+
+	/*$output = array_slice($input, 2, 2);      // returns "c", "d", and "e"
+	print_r($output);*/
+
+	/*input = array("red", "green", "blue", "yellow");
+	array_splice($input, 2);
+	print_r($input);*/
+
+
+	/*$os = array("Mac", "NT", "Irix", "Linux");
+	if (in_array("Irix", $os)) {
+	    echo "Got Irix";
+	}
+	else{
+		echo "We do not have this string";
+	}*/
+
+	//Condition
+	/*$test=10;
+	if($test>7)
+	{
+		echo "Yes it is more than 3";
+	}
+	else if($test>5)
+	{
+		echo "Yes it is more than 5";
+	}
+	else
+	{
+		echo "No this is not more than 3";
+	}*/
+
+	//Switch Case
+	/*$test='ashjdhaksjd';
+	switch($test){
+		case 'val1':
+			//Do this 
+			echo 'This is val1';
+			break;
+		case 'val2':
+			//Do this
+			echo 'This is val2';
+			break;
+		case 'val3':
+			//Do this
+			echo 'This is val3';
+			break;
+		default:
+			//Do something
+			echo 'This is not value';
+			break;
+	}
+*/
+	//Loops
+	/*for($i=1;$i<=5;$i++){
+		echo 'This is a string.' . '<br />';
+	}
+
+	$blogs=['blog1','blog2','blog3'];
+	for($i=0;$i<count($blogs);$i++){
+		echo $blogs[$i] . '<br />';
+	}*/
+
+	//foreach loop
+	//$blogs=['blog1','blog2','blog3'];
+	/*foreach ($blogs as $blog) {
+		// code...
+		echo $blog . '<br />';
+	}*/
+
+
+	//while loop
+	/*$i=0;
+	while($i<count($blogs)){
+		echo $blogs[$i];
+		echo '<br />';
+		$i++;
+	}*/
+
+	//Explode & Implode //String-Array
+
+	//$s1="Mon-Tue-Wed-Thu-Fri";
+	//$days_array = explode('-',$s1);
+	//print_r($days_array);
+
+	//$s2 = implode(',',$days_array);
+	//echo $s2;
+
+
+	//Boolean- true or false data type 1 " "
+
+	//echo false;
+
+	//echo 'John' == 'john'; //false
+
+	//Loose vs strict comparison ===
+	//echo 5==='5'; //false
+
+	// Continue and break
+	/*for($i=1;$i<10;$i++){
+		if($i==6){
+			continue;
+		}
+		if($i==8){
+			break;
+		}
+		echo $i;
+	}*/
+
+	// Function
+	//local scope
+	//global scope
+	//Creation of function
+
+	/*$name='Hiren';
+	function sayHello(){
+		global $name;
+		echo "Hello $name";
+		//$price=10;
+		//echo "Hello ! Good evening! $name ";
+		//echo $price;
+	}
+
+	//echo $price;
+	//calling this function
+	sayHello();*/
+
+
+
+	//Array_Filter
+	/*function odd($var)
+	{
+	    // returns whether the input integer is odd
+	    return $var & 1;
+	}
+
+	function even($var)
+	{
+	    // returns whether the input integer is even
+	    return !($var & 1);
+	}
+
+	$array1 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
+	$array2 = [6, 7, 8, 9, 10, 11, 12];
+
+	echo "Odd :\n";
+	print_r(array_filter($array1, "odd"));
+	echo "Even:\n";
+	print_r(array_filter($array2, "even"));*/
+
+	//Array_Map
+	/*function cube($n)
+	{
+	    return ($n * $n * $n);
+	}
+
+	$a = [1, 2, 3, 4, 5];
+	$b = array_map('cube', $a);
+	print_r($b);*/
+
+
+	/*if(isset($_REQUEST['submit'])){
+		if(!empty($_REQUEST['txtFName']) && !empty($_REQUEST['txtLName'])){
+			echo "FirstName is " . " " . $_REQUEST['txtFName'] . " and LastName is " . " " . $_REQUEST['txtLName'];
+		}
+		else{
+			echo "Fields are empty";
+		}
+	}*/
+
+
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>This is test application.</title>
+</head>
+<body>
+	<!-- This is html comment -->
+	<h1>This is sample form</h1>
+	<!-- <form method="REQUEST" action="index.php">
+		<label>Enter you first name:
+		<input type="text" name="txtFName" />
+		<label>Enter you last name:
+		<input type="text" name="txtLName" />
+		<input type="submit" name="submit" value="submit">
+	</form> -->
+</body>
+</html>
+
+
+
